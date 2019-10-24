482ea861dc85b8ad45fb6b311132a38c83a4ea63
Rafal Kruk <kruku@o2.pl>
2019-10-24 19:09:10 +0200

    Task D.3 D.4 D.5

---
+++ src/main/java/Model/Source.java
@@ -0,0 +1,84 @@
+package Model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "sources")
+public class Source {
+
+    @ManyToMany
+    @JoinTable(
+            name="sources_attached_skills",
+            joinColumns = {@JoinColumn(name="source_id")},
+            inverseJoinColumns = {@JoinColumn(name="skill_id")}
+    )
+    private Set<Skill> skills = new HashSet<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String description;
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public Set<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+//        return super.equals(obj);
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Source source = (Source) obj;
+        return Objects.equals(id, source.id)
+                && Objects.equals(description, source.description)
+                && Objects.equals(name, source.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Source{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/Model/User.java
@@ -0,0 +1,111 @@
+package Model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+
+    @ManyToMany
+    @JoinTable(
+            name="users_known_sources",
+            joinColumns = {@JoinColumn(name="user_id")},
+            inverseJoinColumns = {@JoinColumn(name="source_id")}
+    )
+    private Set<Source> sources = new HashSet<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, name="first_name")
+    private String firstName;
+    @Column(unique = true, name="last_name")
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false, name="username")
+    private String userName;
+
+
+    public Set<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(Set<Source> sources) {
+        this.sources = sources;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+//        return super.equals(obj);
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return Objects.equals(id, user.id)
+                && Objects.equals(firstName, user.firstName)
+                && Objects.equals(lastName, user.lastName)
+                && Objects.equals(password, user.password)
+                && Objects.equals(userName, user.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, password, userName);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
